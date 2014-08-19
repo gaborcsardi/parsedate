05a888fe9cd88e40855db7d36b6b6f41f1649144
Gabor Csardi <csardi.gabor@gmail.com>
2014-08-19 18:57:03 -0400

    Proper DESCRIPTION and sketch of the package

---
+++ R/parsedate-package.r
@@ -0,0 +1,19 @@
+## --------------------------------------------------------------------
+#' Parse date from any format
+#'
+#' @docType package
+#' @name parsedate-package
+
+NULL
+
+## --------------------------------------------------------------------
+#' Parse date from any format
+#'
+#' @param dates A character vector.
+#' @return A \code{POSIXct} vector.
+#'
+#' @export
+
+parse_date <- function(dates) {
+
+}
