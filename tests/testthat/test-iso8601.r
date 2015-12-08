67240231d5ed7087bbde6ed1d946cbf2e68af599
Gabor Csardi <csardi.gabor@gmail.com>
2015-12-08 09:24:12 +0000

    Fix non-matchinfg ISO patterns tests

--- tests/testthat/test-iso8601.r
+++ tests/testthat/test-iso8601.r
@@ -140,7 +140,7 @@ test_that("Pelago non-matching", {
   ))
 
   apply(tests, 1, function(x) {
-    d <- format_iso_8601(parse_iso_8601(x[1]))
+    d <- parse_iso_8601(x[1])
     expect_equal(d, as.POSIXct(NA))
   })
 
